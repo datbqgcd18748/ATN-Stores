fdfd83eacb57887c56f189e5500dc8dc781823bd
buiquocdatgl2000 <buiquocdatgl2000@gmail.com>
2020-06-20 12:26:17 +0700

    first commit

---
+++ store/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    Home,
+    Appointment,
+    Booking,
+    SearchResultsView,
+    View
+
+)
+app_name = 'store'
+
+
+urlpatterns = [
+    path('', Home, name='home'),
+    path('booking/', Booking, name="booking"),
+    path('appointment', Appointment, name="appointment"),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('view', View, name='view'),
+]
